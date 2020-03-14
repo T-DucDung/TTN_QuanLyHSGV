00604fbe50d5f2e8aedb34a5f388248a06170309
9tee <50616358+9tee@users.noreply.github.com>
2020-03-14 15:37:28 +0700

    Interface implement
    tạo interface để convert

---
+++ TTN-QL-HSGV/BUS/Converter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTN_QL_HSGV.BUS
+{
+    interface IConverter<T>
+    {
+        List<T> Convert(DataTable datatable);
+    }
+}
