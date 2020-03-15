722eaefbcc724643f442881201946f99e19ef925
9tee <50616358+9tee@users.noreply.github.com>
2020-03-15 16:26:56 +0700

    comparer part 1

---
+++ TTN-QL-HSGV/DTO/LopComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTN_QL_HSGV.DTO
+{
+    class LopComparer : IComparer<Lop>
+    {
+
+        private string memberName = string.Empty;
+        private SortOrder sortOrder = SortOrder.None;
+
+        public LopComparer(string memberName, SortOrder sortOrder)
+        {
+            this.memberName = memberName;
+            this.sortOrder = sortOrder;
+        }
+
+        public int Compare(Lop x, Lop y)
+        {
+            switch(memberName)
+            {
+
+            }
+        }
+    }
+}
